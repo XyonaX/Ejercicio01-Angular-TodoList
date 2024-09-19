c448bdf149417e09e9ba7d8da26aa7a229ba76ce
Vargas Jonatan <jonatan.vargasportillo@gmail.com>
2024-09-19 14:28:30 -0300

    fix

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -10,7 +10,7 @@ import { TaskModel } from '../../models/taks.model';
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
 
   tasks: TaskModel[] = [];
 
