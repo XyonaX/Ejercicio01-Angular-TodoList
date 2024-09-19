81cf363375e3c128ee51b0ae7e8f28d96f5ec363
Jonatan <jonatan.vargasportillo@gmail.com>
2024-09-18 21:24:16 -0300

    se agrego persistencia al localStorage

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
 import { TaskModel } from '../../models/taks.model';
 
@@ -14,21 +14,41 @@ export class TodoListComponent {
 
   tasks: TaskModel[] = [];
 
+  private storageKey = 'todo-list';
+
+  ngOnInit(){
+    this.loadTasksFromLocalStorage();
+  }
+
   handleTaskCompleted(id: number){
     const task = this.tasks.find(t => t.id === id);
     if(task) {
       task.completed = !task.completed;  
+      this.saveTasksToLocalStorage();
     }
 
   }
 
   handleTaskDeleted(id: number){
     this.tasks = this.tasks.filter(t => t.id !== id);
+    this.saveTasksToLocalStorage();
   }
 
   addTask( title: string ){
     const newTask = new TaskModel(this.tasks.length + 1, title);
     this.tasks.push(newTask);
+    this.saveTasksToLocalStorage();
+  }
+
+  private saveTasksToLocalStorage(){
+    localStorage.setItem(this.storageKey, JSON.stringify(this.tasks));
+  }
+
+  private loadTasksFromLocalStorage(){
+    const storedTasks = localStorage.getItem(this.storageKey);
+    if(storedTasks){
+      this.tasks = JSON.parse(storedTasks);
+    }
   }
 
 }
